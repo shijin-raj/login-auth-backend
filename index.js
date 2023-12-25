e89e46489f4ec0320d8acf2cfe728e2d68bd16d5
Shijin Raj <shijin@riafy.me>
2023-12-25 22:43:19 +0530

    Refresh token workflow updated

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
-const cookieParser = require('cookie-parser');
-const cors = require('cors'); // Import the cors middleware
-require('dotenv').config();
+const cookieParser = require("cookie-parser");
+const cors = require("cors"); // Import the cors middleware
+require("dotenv").config();
 const userModel = require("./userModel");
 const app = express();
 const PORT = process.env.PORT;
 
 const corsOptions = {
-    origin: process.env.ORIGIN_URL,
-    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
-  };
+  origin: process.env.ORIGIN_URL,
+  credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+};
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
-
-
 // Function to generate a JWT token
 const generateAccessToken = (userId) => {
-  return jwt.sign({ userId }, process.env.ACCESS_SECRET_KEY, { expiresIn: "15m" });
+  return jwt.sign({ userId }, process.env.ACCESS_SECRET_KEY, {
+    expiresIn: "1m",
+  });
 };
 
 // Function to generate a refresh token
 const generateRefreshToken = (userId) => {
-  return jwt.sign({ userId }, process.env.REFRESH_SECRET_KEY, { expiresIn: "7d" });
+  return jwt.sign({ userId }, process.env.REFRESH_SECRET_KEY, {
+    expiresIn: "7d",
+  });
 };
 
 // Middleware to verify the access token
 const verifyAccessToken = (req, res, next) => {
-    console.log(req);
+  console.log(req);
   const accessToken = req.cookies.accessToken;
 
   if (!accessToken) {
@@ -41,7 +43,7 @@ const verifyAccessToken = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(accessToken,process.env.ACCESS_SECRET_KEY);
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_SECRET_KEY);
     req.user = decoded;
     next();
   } catch (err) {
@@ -63,54 +65,87 @@ app.post("/login", (req, res) => {
   const refreshToken = generateRefreshToken(user.id);
 
   // Set cookies with tokens
-  res.cookie("accessToken", accessToken, { httpOnly: true, sameSite: 'strict' });
-  res.cookie("refreshToken", refreshToken, { httpOnly: true, sameSite: 'strict' });
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    sameSite: "strict",
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "strict",
+  });
 
   res.json({ message: "Login successful" });
 });
 
-
 // Route to refresh the access token using the refresh token
 app.post("/refresh", (req, res) => {
-  const refreshToken = req.cookies.refreshToken;
+  const hasAccessToken = req.cookies.accessToken !== undefined;
+  const hasRefreshToken = req.cookies.refreshToken !== undefined;
 
-  if (!refreshToken) {
-    return res.status(401).json({ message: "No refresh token provided" });
+  checkRefreshToken = false;
+
+  if (hasAccessToken) {
+    const accessToken = req.cookies.accessToken;
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_SECRET_KEY);
+      req.user = decoded;
+      res.json({ message: "Login successful", user: req.user, status: true });
+    } catch (err) {
+      checkRefreshToken = true;
+    }
+  } else {
+    checkRefreshToken = true;
   }
 
-  // Verify the refresh token
-  try {
-    const decoded = jwt.verify(refreshToken,  process.env.REFRESH_SECRET_KEY);
+  if (checkRefreshToken) {
+    if (hasRefreshToken) {
+      const refreshToken = req.cookies.refreshToken;
+      // Verify the refresh token
+      try {
+        const decoded = jwt.verify(
+          refreshToken,
+          process.env.REFRESH_SECRET_KEY
+        );
 
-    // Generate a new access token
-    const newAccessToken = generateAccessToken(decoded.userId);
+        // Generate a new access token
+        const newAccessToken = generateAccessToken(decoded.userId);
 
-    // Set the new access token in a new cookie
-    res.cookie("accessToken", newAccessToken, { httpOnly: true });
+        // Set the new access token in a new cookie
 
-    res.json({ message: "Token refreshed successfully" });
-  } catch (err) {
-    res.status(401).json({ message: "Invalid refresh token" });
+        res.cookie("accessToken", newAccessToken, {
+          httpOnly: true,
+          sameSite: "strict",
+        });
+
+        res.json({ message: "Token refreshed successfully", status: true });
+      } catch (err) {
+        res
+          .status(200)
+          .json({ message: "Invalid refresh token", status: false });
+      }
+    } else {
+      return res
+        .status(200)
+        .json({ message: "No refresh token provided", status: false });
+    }
   }
 });
 
 // Example of a protected route
 app.get("/protected", verifyAccessToken, (req, res) => {
-    const data ={
-        likes :10,
-        comments:20,
-        shares:100
-    };
-  res.json({ message: "This is a protected route", user: req.user ,data});
+  const data = {
+    likes: 10,
+    comments: 20,
+    shares: 100,
+  };
+  res.json({ message: "This is a protected route", user: req.user, data });
 });
 
-app.post("/loginCheck", verifyAccessToken, (req, res) => {
-res.json({ message: "Login successful", user: req.user});
-});
+// app.post("/loginCheck", verifyAccessToken, (req, res) => {
+//   res.json({ message: "Login successful", user: req.user });
+// });
 
-
-
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   // Expire the HTTP-only secure cookie
 
   // Check if the HTTP-only secure cookies exist
@@ -119,14 +154,24 @@ app.post('/logout', (req, res) => {
 
   // Clear the HTTP-only secure cookies if they exist
   if (hasAccessToken) {
-    res.clearCookie('accessToken', { httpOnly: true, secure: true, sameSite: 'strict', path: '/' });
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      path: "/",
+    });
   }
 
   if (hasRefreshToken) {
-    res.clearCookie('refreshToken', { httpOnly: true, secure: true, sameSite: 'strict', path: '/' });
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      path: "/",
+    });
   }
   // Send a response
-  res.json({ message: "Logged out successfully"});
+  res.json({ message: "Logged out successfully" });
 });
 
 app.listen(PORT, () => {
